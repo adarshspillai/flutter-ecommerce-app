2cf190a3577e4bbf0951b1cfa3085c9a620e9ae0
adarshspillai <adarshspillai99@gmail.com>
2025-12-29 23:47:04 +0530

    Fix API handling and disable minify for release APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,8 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
+            isMinifyEnabled = false
+            isShrinkResources = false            // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
